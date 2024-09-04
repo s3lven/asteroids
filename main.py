142a7885e928af8b38d24f7f9980550f0a600937
Eriz <erizjsub2@gmail.com>
2024-09-04 00:19:24 -0700

    feat: added asteroids, player object, asteroid spawning rules

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main ():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Instantiate a player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(x, y)
+    asteroidField = AsteroidField()
 
     while True:
         # Check if they want to quit game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Check for player rotations
+        for object in updatable:
+            object.update(dt)
 
-        # Otherwise, render black screen
+        for object in asteroids:
+            if object.isCollision(player):
+                print("Game over!")
+                exit()
+            
+        # Draw black screen and player
         screen.fill('black')
+        for object in drawable:
+            object.draw(screen)
+
+        # Then render (THIS MUST BE LAST)
         pygame.display.flip()
+        # Update the clock
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
